036676e90bd1b2112df79a42fd93d02ff840695e
Crackstein <rico47198@live.de>
2024-02-15 14:10:54 +0100

    dev 0.1.5

--- src/buttonListener.ts
+++ src/buttonListener.ts
@@ -1,13 +1,36 @@
+import { currencyConversion } from "./currencyConversion";
 import { updateElementValue } from "./gameService";
 
 export const setupDelegatedButtonListener = () => {
-  document.body.addEventListener("click", (event) => {
+  document.body.addEventListener("click", async (event) => {
     if (!(event.target instanceof Element)) return;
     const targetElement = event.target.closest("button");
 
     if (targetElement && targetElement.dataset.test === "bet-button") {
       console.log("Bet button clicked");
-      updateElementValue();
+      await updateElementValue();
+      // Stellen Sie sicher, dass das zweite Element aktualisiert wurde, bevor Sie den Dollar-Wert aktualisieren
+      const secondElementOverlay = document.querySelector(
+        'input[data-test="profit-input"]'
+      ) as HTMLInputElement;
+
+      // Stellen Sie sicher, dass das Element existiert, bevor Sie fortfahren
+      if (secondElementOverlay) {
+        const dollarConversionInstance = new currencyConversion(
+          "span.label-content.full-width.svelte-1vx6ykn > div.right-align.svelte-5v1hdl > div.currency-conversion.svelte-eh26te > div.svelte-eh26te"
+        );
+
+        // Beobachten Sie den Wert von secondElementOverlay und aktualisieren Sie den Dollarwert
+        let lastValue = secondElementOverlay.value;
+        setInterval(() => {
+          const currentValue = secondElementOverlay.value;
+          if (currentValue !== lastValue) {
+            const secondValue = parseFloat(currentValue) || 0;
+            dollarConversionInstance.updateDisplay(secondValue);
+            lastValue = currentValue;
+          }
+        }, 100); // Überprüfen Sie den Wert alle 200ms
+      }
     } else if (
       targetElement &&
       targetElement.dataset.test === "cashout-button"
--- src/currencyConversion.ts
+++ src/currencyConversion.ts
@@ -16,3 +16,22 @@ export class currencyConversion {
     this.displayElement.textContent = `$${dollarValue.toFixed(2)}`;
   }
 }
+
+// const dollarValueDisplay = document.querySelector(
+//   "span.label-content.full-width.svelte-1vx6ykn > div.right-align.svelte-5v1hdl > div.currency-conversion.svelte-eh26te > div.svelte-eh26te"
+// );
+
+// // Funktion, die den Wert von secondElementOverlay liest und den Dollarwert aktualisiert
+// export function updateDollarValueBasedOnSecondElement(
+//   secondElementOverlay: HTMLInputElement
+// ) {
+//   const secondValue = parseFloat(secondElementOverlay.value) || 0;
+//   if (dollarValueDisplay) {
+//     (dollarValueDisplay as unknown as currencyConversion).updateDisplay(
+//       secondValue
+//     );
+//   }
+// }
+
+// // setInterval, um den Wert regelmäßig zu überprüfen und den Dollarwert zu aktualisieren
+// setInterval(updateDollarValueBasedOnSecondElement, 200); // Überprüft alle 200ms
